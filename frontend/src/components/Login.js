0e8430c9a2d2f1f1cd1eaf1efcf0fa2ffec7c620
Emil <emil.helix@gmail.com>
2025-01-09 22:00:08 +0100

    Login page designed, but not implemented with backend

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -14,6 +14,7 @@ const Login = () => {
         <div className="input-box">
           <input 
           placeholder="Enter your password..." 
+          type="password"
           required />
         </div>
 
