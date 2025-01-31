f2e19338300ed9de24cf3e70cdda5cc2b7be5440
Emil Eriksson <emil.helix@gmail.com>
2025-01-31 14:57:53 +0100

    Display of leaderboard working correctly, somehow may have used a bit of chatgpt hihi

--- frontend/src/components/Leaderboard.js
+++ frontend/src/components/Leaderboard.js
@@ -6,34 +6,38 @@ const USERS_URL = "/users"
 const Leaderboard = () => {
   const [users, setUsers] = useState([])
 
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get(USERS_URL);
+      const data = response.data
+      setUsers(data);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const sortUsers = (users) => {
+
+  }
+
   useEffect(() => {
     console.log("Attempting data fetch");
-    const fetchUsers = async () => {
-      try {
-        const response = await axios.get(USERS_URL);
-        const data = response.data
-        setUsers(data);
-        console.log(data);
-      } catch (err) {
-        console.log(err);
-      }
-    }
     fetchUsers();
   });
 
+  const sortedUsers = users.sort(
+    (a, b) => parseFloat(b.accountBalance) - parseFloat(a.accountBalance)
+  );
+  
+  console.log("sorted");
+
   return (
-    <div className="wrapper-leaderboard">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
-<p>Ad dolore dignissimos asperiores dicta facere optio quod commodi nam tempore recusandae. Rerum sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio aliquam!</p>
-<p>Tenetur quod quidem in voluptatem corporis dolorum dicta sit pariatur porro quaerat autem ipsam odit quam beatae tempora quibusdam illum! Modi velit odio nam nulla unde amet odit pariatur at!</p>
-<p>Consequatur rerum amet fuga expedita sunt et tempora saepe? Iusto nihil explicabo perferendis quos provident delectus ducimus necessitatibus reiciendis optio tempora unde earum doloremque commodi laudantium ad nulla vel odio?</p>
-      Leaderboard
-      {users.map( (data) => (
-        <div> {data.username} </div>
+    <div className="wrapper_leaderboard">
+      {sortedUsers.map( (user) => (
+        <div>  <span> {user.username} </span> <span> {user.accountBalance} </span> </div>
       ))}
-      end of leaderboard
     </div>
-    
   )
 }
 
